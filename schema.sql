32afb2117bdfa319a25b424974962260867ea270
AndyPicton <andyspicton@yahoo.com>
2022-04-12 18:19:20 -0500

    New PH Analysis Files

--- schema.sql
+++ schema.sql
@@ -31,25 +31,23 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR(30) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (dept_no, emp_no)
 );
-SELECT * FROM departments;
-
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no, title, from_date)
+);
 
 
 
